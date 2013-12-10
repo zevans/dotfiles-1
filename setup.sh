e31ab442f321b919b6d4ca8b38e17d92ec06030a
josephlogik <joseph@logik.com>
2013-12-09 23:15:30 -0800

    Start migrating rake tasks to pure bash

--- setup.sh
+++ setup.sh
@@ -1,20 +1,112 @@
-command -v curl >/dev/null 2>&1 || { echo "Installing curl (using sudo):"; sudo apt-get install curl; }
-command -v rvm >/dev/null 2>&1 || { echo "Installing rvm";
-                                    curl -sSL https://get.rvm.io | bash -s stable;
-									echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile; }
-source ~/.bash_profile
-rvm install ruby
-rvm use ruby
+#!/bin/bash
 
-gem install rake
-rake setup
+E_ARGUMENT=11 # change later?
 
-echo 'Building Command-T'
-cd dotvim/bundle/Command-T/ruby/command-t/
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-rvm use system
-ruby extconf.rb
-make
-echo "...done!"
+linux_check_pkg () {
+  if [ -z "$1" ]; then return "$E_ARGUMENT"; fi
+  dpkg -s "$1" 2>&1 | grep -q "install ok installed"
+}
 
-echo 'Close existing terminals or `source ~/.bash_profile`'
+linux_install_pkg () {
+  if [ -z "$1" ]; then return "$E_ARGUMENT"; fi
+  if linux_check_pkg "$1"; then
+	echo "$1 already installed... skipping"
+  else
+	sudo apt-get install "$1"
+  fi
+}
+
+linux_setup () {
+
+  # Return unless specific version is supported
+  if [[ -e /etc/os-release ]]; then
+	source /etc/os-release
+	case "$VERSION_ID" in
+	  13.10 ) echo "Detected Ubuntu 13.10..." ;;
+	  12.04 ) echo "Detected Ubuntu 12.04..." ;;
+	  10.04 ) echo "Detected Ubuntu 10.04..." ;;
+	  *     ) echo "Unsupported Linux; skipping setup"; return ;;
+	esac
+  else
+	echo "Unsupported Linux; skipping setup"; return
+  fi
+
+  echo -e "\nStarting package installation (may require sudo password)"
+  linux_install_pkg "ack-grep"
+  linux_install_pkg "exuberant-ctags"
+  linux_install_pkg "xclip"
+  linux_install_pkg "vim-gtk"
+  linux_install_pkg "ruby"
+  linux_install_pkg "ruby1.9.1-dev"
+  linux_install_pkg "ruby1.8-dev"
+  echo -e "Package installation complete!\n"
+
+}
+
+setup () {
+  echo "Starting $OSTYPE setup..."
+  case "$OSTYPE" in
+	linux-gnu ) linux_setup  ;;
+	darwin*   ) osx_setup    ;;
+	cygwin    ) cygwin_setup ;;
+  esac
+  echo "Setup for $OSTYPE complete!"
+}
+
+setup
+
+safe_link () {
+  if [ -z "$1" ]; then return "$E_ARGUMENT"; fi
+  if [ -z "$2" ]; then target=$1; else target=$2; fi
+  # If not a symbolic link or directory, backup first
+  if [[ ! -L "$HOME/.$1" && ! -d "$HOME/.$1"  && -e $HOME/.$1 ]]; then
+	echo "Making backup of $HOME/.$1 at $HOME/.${1}.dotfile_backup"
+	mv "$HOME/.$1" "$HOME/.${1}.dotfile_backup"
+  fi
+  echo "linking $1 to $target"
+  ln -sf "$HOME/dotfiles/$1" "$HOME/.$target"
+}
+
+dotfile_links () {
+  echo "Linking dotfiles..."
+  safe_link "bash_profile"
+  safe_link "bashrc"
+  safe_link "vimrc"
+  safe_link "vimrc.local"
+  safe_link "vimrc.bundles"
+  safe_link "vimrc.bundles.local"
+  safe_link "gvimrc"
+  safe_link "dotvim" "vim"
+  safe_link "tmux.conf"
+  safe_link "ackrc"
+  safe_link "gitconfig"
+  safe_link "bash/inputrc" "inputrc"
+  safe_link "gemrc"
+  safe_link "minttyrc"
+  safe_link "screenrc"
+  echo "Dotfile linking complete!"
+}
+
+dotfile_links
+
+#command -v curl >/dev/null 2>&1 || { echo "Installing curl (using sudo):"; sudo apt-get install curl; }
+#command -v rvm >/dev/null 2>&1 || { echo "Installing rvm";
+                                    #curl -sSL https://get.rvm.io | bash -s stable;
+									#echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >> ~/.bash_profile; }
+#source ~/.bash_profile
+#rvm install ruby
+#rvm use ruby
+
+#gem install rake
+#rake setup
+
+#echo 'Building Command-T'
+#cd dotvim/bundle/Command-T/ruby/command-t/
+#[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+#rvm use system
+#ruby extconf.rb
+#make
+#echo "...done!"
+
+#echo 'Close existing terminals or `source ~/.bash_profile`'
+
